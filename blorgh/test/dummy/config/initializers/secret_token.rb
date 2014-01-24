b3062f60d04454ec3fa83b7940ce3f4ccb4727d5
piyushanchanto <piyush.awasthi@anchanto.com>
2014-01-24 18:59:10 +0530

    Intial Commit

---
+++ blorgh/test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'cc9ce608c2c1d448b0e75d2098efb524c87424ed04398f7ecac5faed2e00349831de157654557843061589e34a3514edfea509f77b23f3aa0f7260aef24f1f4d'
